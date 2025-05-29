0d763ab70f7cbc1541e4c67da644fb38177f8ae2
lolololololololololololololololololl <43680@sbc.vic.edu.au>
2025-05-29 11:43:38 +1000

    Update somerandomfile.py

--- Hugo's work/somerandomfile.py
+++ Hugo's work/somerandomfile.py
@@ -25,3 +25,15 @@ for i in range(10):
     sys.stdout.write("\rloading please wait" + "." * (i % 4))
     sys.stdout.flush()
     time.sleep(0.5)
+    
+print("\n" + Fore.LIGHTGREEN_EX + "Username successful" + Style.RESET_ALL)
+time.sleep(2)
+print("To verify who you are enter you password")
+
+password = input ("Enter your password for:")
+if password == 'ABC123':
+    print(Fore.LIGHTGREEN_EX + "credentials correct, entering system!" + Style.RESET_ALL)
+else:
+    print(Fore.RED + "credentials incorrect, logging out!" + Style.RESET_ALL)
+if response == 'up':
+     create_user = input ("Greetings new customer. Please enter your desired username!")
